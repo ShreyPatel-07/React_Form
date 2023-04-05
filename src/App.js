6ea78488b29cd27b831aefd8c47ad5ea7a6cc2da
SHREYKUMAR SURESHBHAI PATEL <124682881+ShreyPatel-07@users.noreply.github.com>
2023-04-05 16:24:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Leftside from "./components/Leftside";
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <div className="App">
+      <Leftside />
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Leftside.js
@@ -0,0 +1,22 @@
+import "../components/Leftside.css";
+
+const LeftSide = () => {
+  return (
+    <>
+      <div className="just_to_read">
+        <div className="para1">
+          <p>
+            Learn to code by <br /> watching others
+          </p>
+        </div>
+        <div className="para2">
+          <p>See how experienced developers solve problems in real-time.</p>
+          <p>Watching scripted tutorials is great, but understanding how</p>
+          <p>developers think is invaluable.</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LeftSide;
